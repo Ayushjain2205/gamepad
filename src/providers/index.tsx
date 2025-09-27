5e6e4a0e57294fea0b8b2bea40158ada521cfa21
Ayush Jain <ayushjain2205@gmail.com>
2025-09-27 09:09:08 +0530

    Disable Eruda for prod

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,13 +1,13 @@
-'use client';
-import { MiniKitProvider } from '@worldcoin/minikit-js/minikit-provider';
-import { Session } from 'next-auth';
-import { SessionProvider } from 'next-auth/react';
-import dynamic from 'next/dynamic';
-import type { ReactNode } from 'react';
+"use client";
+import { MiniKitProvider } from "@worldcoin/minikit-js/minikit-provider";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import dynamic from "next/dynamic";
+import type { ReactNode } from "react";
 
 const ErudaProvider = dynamic(
-  () => import('@/providers/Eruda').then((c) => c.ErudaProvider),
-  { ssr: false },
+  () => import("@/providers/Eruda").then((c) => c.ErudaProvider),
+  { ssr: false }
 );
 
 // Define props for ClientProviders
@@ -33,10 +33,10 @@ export default function ClientProviders({
   session,
 }: ClientProvidersProps) {
   return (
-    <ErudaProvider>
-      <MiniKitProvider>
-        <SessionProvider session={session}>{children}</SessionProvider>
-      </MiniKitProvider>
-    </ErudaProvider>
+    // <ErudaProvider>
+    <MiniKitProvider>
+      <SessionProvider session={session}>{children}</SessionProvider>
+    </MiniKitProvider>
+    // </ErudaProvider>
   );
 }
